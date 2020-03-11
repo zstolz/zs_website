de1e4310120bfb1ace8261e980c7378b8af20875
Zack Stolz <zstolz26@example.com>
2020-03-10 20:18:23 -0400

    PHP form sub & alert - style updates

--- contact.php
+++ contact.php
@@ -3,7 +3,7 @@
 
 if($_POST["submit"]) {
     $recipient="zstolz26@gmail.com";
-    $subject="Website Message";
+    $subject="Contact form message";
     $sender=$_POST["firstName"]." ".$_POST["lastName"];                       
     $company=$_POST["company"];
     $phone=$_POST["phone"];
@@ -12,8 +12,7 @@ if($_POST["submit"]) {
     $mailBody="Name: $sender \r\n";
     $mailBody.="Company: $company \r\n";
     $mailBody.="Phone: $phone \r\n";
-    $mailBody.="Email: $senderEmail\r\n";
-    $mailBody.= "Message: $message\r\n";
+    $mailBody.="Email: $senderEmail\r\n\n$message";
     mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
     $thankYou="<p>Thank you! Your message has been sent.</p>";
 }
